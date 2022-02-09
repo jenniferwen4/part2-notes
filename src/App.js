e41abd41fb1c8d66bda0da2370c9257cadb0a8ad
Jen <jenniferwen4@gmail.com>
2022-02-09 18:04:44 +1100

    create loginForm component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import Note from "./components/Note";
 import Notification from "./components/Notification";
 import Footer from "./components/Footer";
+import loginForm from "./components/loginForm";
 
 import noteService from "./services/notes";
 import loginService from "./services/login";
@@ -94,30 +95,6 @@ const App = () => {
       }, 5000);
     }
   };
-
-  const loginForm = () => (
-    <form onSubmit={handleLogin}>
-      <div>
-        username
-        <input
-          type="text"
-          value={username}
-          name="Username"
-          onChange={({ target }) => setUsername(target.value)}
-        />
-      </div>
-      <div>
-        password
-        <input
-          type="password"
-          value={password}
-          name="Password"
-          onChange={({ target }) => setPassword(target.value)}
-        />
-      </div>
-      <button type="submit">login</button>
-    </form>
-  );
 
   const noteForm = () => (
     <form onSubmit={addNote}>
