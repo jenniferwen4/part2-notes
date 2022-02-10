4ff19b5d4c713779f2e5713556381fe8a6672179
Jen <jenniferwen4@gmail.com>
2022-02-10 13:38:44 +1100

    Forms are all working again

--- src/App.js
+++ src/App.js
@@ -71,7 +71,6 @@ const App = () => {
   };
 
   const handleNoteChange = (event) => {
-    console.log(event.target.value);
     setNewNote(event.target.value);
   };
 
--- src/components/NoteForm.js
+++ src/components/NoteForm.js
@@ -1,15 +1,15 @@
 import React from "react";
 
-const NoteForm = ({ onSubmit, handleNoteChange, value }) => {
+const NoteForm = ({ onSubmit, handleChange, value }) => {
   return (
     <div>
       <h2>Create a new note</h2>
+
       <form onSubmit={onSubmit}>
-        <input value={value} onChange={handleNoteChange} />
+        <input value={value} onChange={handleChange} />
         <button type="submit">save</button>
       </form>
     </div>
   );
 };
-
 export default NoteForm;
