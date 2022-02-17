618d5b0a57b5153222d3586a0e3ca71d068dc36b
Jen <jenniferwen4@gmail.com>
2022-02-17 17:19:53 +1100

    add login fail test and login comman

--- cypress/integration/note_app.spec.js
+++ cypress/integration/note_app.spec.js
@@ -17,6 +17,18 @@ describe("Note app", function () {
     );
   });
 
+  it.only("login fails with wrong password", function () {
+    cy.contains("login").click();
+    cy.get("#username").type("booty");
+    cy.get("#password").type("wrong");
+    cy.get("#login-button").click();
+
+    cy.get(".error")
+      .should("contain", "wrong credentials")
+      .and("have.css", "color", "rgb(255, 0, 0)")
+      .and("have.css", "border-style", "solid");
+  });
+
   it("login form can be opened", function () {
     cy.contains("login").click();
   });
@@ -32,19 +44,27 @@ describe("Note app", function () {
 
   describe("when logged in", function () {
     beforeEach(function () {
-      cy.contains("login").click();
-      cy.get("input:first").type("booty");
-      cy.get("input:last").type("open");
-      cy.get("#login-button").click();
+      cy.login({ username: "mluukkai", password: "salainen" });
     });
     it("a new note can be created", function () {
       cy.contains("new note").click();
       cy.get("input").type("a note created by cypress");
       cy.contains("save").click();
+      cy.contains("show all").click();
+
       cy.contains("a note created by cypress");
+    });
+    describe("and a note exists", function () {
+      beforeEach(function () {
+        cy.contains("show all").click();
+        cy.contains("new note").click();
+        cy.get("input").type("another note cypress");
+        cy.contains("save").click();
+      });
+      it("it can be made important", function () {
+        cy.contains("another note cypress").contains("make important").click();
+        cy.contains("another note cypress").contains("make not important");
+      });
     });
   });
 });
-
-// something wrong with back end
-// not 'getting' notes
